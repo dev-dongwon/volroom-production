e357893e8fbc1b6f60acda711a012f2ddc0c41e2
dev-dongwon <dev.dongwonkim@gmail.com>
2019-10-15 19:08:02 +0900

    Feat : redis host, port 설정

--- server/redis/redis-connect.js
+++ server/redis/redis-connect.js
@@ -1,5 +1,8 @@
 const redis = require("async-redis");
-const client = redis.createClient();
+const client = redis.createClient({
+  host: "redis.cr3cu4.ng.0001.apn2.cache.amazonaws.com",
+  port: "6379"
+});
 
 client.on("connect", () => {
   console.log("redis connected");
