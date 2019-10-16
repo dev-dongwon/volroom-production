9b5b29739ce12f32940908a7e54fa5ff5fba1620
dev-dongwon <dev.dongwonkim@gmail.com>
2019-10-16 20:30:32 +0900

    Feat : 외부 API 서버 대신 local server로 교체

--- client/src/utils/axios-api.js
+++ client/src/utils/axios-api.js
@@ -1,7 +1,7 @@
 import Axios from "axios";
 
 const axios = Axios.create({
-  baseURL: "https://server-dev.ap-northeast-2.elasticbeanstalk.com/"
+  baseURL: "http://localhost:8081/"
 });
 
 export default axios;
