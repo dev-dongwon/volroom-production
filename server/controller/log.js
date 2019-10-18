fadab79111f72d3265d0e26712cf1af68d5ff8eb
dev-dongwon <dev.dongwonkim@gmail.com>
2019-10-18 18:10:37 +0900

    Fix : 불필요한 console.log 삭제

--- server/controller/log.js
+++ server/controller/log.js
@@ -3,7 +3,6 @@ const LoginLog = require("../mongoDB/models/login-log");
 const logController = {
   getUserLog: async (req, res, next) => {
     const result = await LoginLog.find();
-    console.log(result);
     return res.json(result);
   }
 };
