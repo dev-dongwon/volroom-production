4abf7968df5eebf7f5603f1079f93aab123063bb
dev-dongwon <dev.dongwonkim@gmail.com>
2019-10-20 12:48:29 +0900

    Fix : build 파일 제거

--- client/src/utils/axios-api.js
+++ client/src/utils/axios-api.js
@@ -1,7 +1,9 @@
 import Axios from "axios";
 
 const axios = Axios.create({
-  baseURL: "https://www.volroomup.space/"
+  // baseURL: "https://www.volroomup.space/"
+    baseURL: "http://localhost:8081/"
+
 });
 
 export default axios;
